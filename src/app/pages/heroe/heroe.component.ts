fea74d08f7d85d58d04cb1f57bcbc596f801aa26
Carlos <carlos@carlos.com>
2020-07-13 09:59:11 -0500

    lista-de-heroes-firebase

---
+++ src/app/pages/heroe/heroe.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroeModel } from '../../models/heroe.model';
+import { NgForm } from '@angular/forms';
+import { HeroesService } from '../../services/heroes.service';
+
+import Swal from 'sweetalert2';
+import { Observable } from 'rxjs';
+
+
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrls: ['./heroe.component.css']
+})
+export class HeroeComponent implements OnInit {
+
+  heroe = new HeroeModel();
+
+  constructor( private heroesService: HeroesService ) { }
+
+  ngOnInit(): void {
+  }
+
+  guardar( form: NgForm ) {
+
+    if ( form.invalid ) {
+      console.log('Formulario no válido');
+      return;
+    }
+
+    Swal.fire({
+      title: 'Espere',
+      text: 'Guardando información',
+      icon: 'info',
+      allowOutsideClick: false
+    });
+    Swal.showLoading();
+
+    let peticion: Observable<any>;
+    
+
+    if ( this.heroe.id ) {
+
+      peticion = this.heroesService.actualizarHeroe( this.heroe );
+
+    } else {
+
+      peticion = this.heroesService.crearHeroe( this.heroe );
+
+    }
+
+    peticion.subscribe( resp => {
+
+      Swal.fire({
+        title: this.heroe.nombre,
+        text: 'Se actualizó correctamente',
+        icon: 'success'
+      });
+
+    });
+
+    
+
+  }
+
+}
