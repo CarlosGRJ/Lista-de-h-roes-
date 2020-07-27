3f240aa3878c20e12f2f87897cd89d2a2f402ed8
Carlos <carlosgrj2013@gmail.com>
2020-07-27 15:48:43 -0500

    Delete y Editar

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HeroeModel } from '../models/heroe.model';
-import { map } from 'rxjs/operators';
+import { map, delay } from 'rxjs/operators';
 
 
 @Injectable({
@@ -36,6 +36,44 @@ export class HeroesService {
     delete heroeTemp.id;
 
     return this.http.put(`${ this.url }/heroes/${ heroe.id }.json`, heroeTemp);
+
+  }
+
+  borrarHeroe( id: string ) {
+
+    return this.http.delete(`${ this.url }/heroes/${ id }.json`);
+
+  }
+
+  getHeroe( id: string) {
+
+    return this.http.get(`${ this.url }/heroes/${ id }.json`);
+
+  }
+
+  getHeroes() {
+    return this.http.get(`${ this.url }/heroes.json`)
+              .pipe(
+                map( this.crearArreglo ),
+                delay(0)
+              );
+  }
+
+  private crearArreglo( heroesObj: object ) {
+
+    const heroes: HeroeModel[] = [];
+
+    if ( heroesObj === null ) { return []; }
+
+    Object.keys( heroesObj ).forEach( key => {
+
+      const heroe: HeroeModel = heroesObj[key];
+      heroe.id = key;
+
+      heroes.push( heroe );
+    });
+
+    return heroes;
 
   }
 
